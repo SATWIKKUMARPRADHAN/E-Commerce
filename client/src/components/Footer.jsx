372ca4f5e6008c20a31d60079421f444e5417465
shalini <hey@gamil.com>
2025-12-22 15:25:36 +0530

    added bcypt in server.js for safe login and signup changes the logo of the tab

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,12 +6,13 @@ import { Pointer } from 'lucide-react'
 
 
 
+
 export default function Footer() {
   return (
     <div className="container-fluid p-3 mt-5" style={{boxShadow:'0 0px 5px yellow', borderRadius: '5px'}}>
       <div className="row p-4" style={{ backgroundColor: '#0a0a0adc' }}>
         
-        <div className="col text-white">
+        <div className="col text-white fw-bold">
           <p className="fw-bold fs-5">Help</p>
           <a className="text-white d-block" style={{textDecoration: 'none', cursor: 'pointer'}} href='/help'>Help</a>
           <a className="text-white d-block" style={{textDecoration: 'none'}} href='/aboutUs'>About us</a>
@@ -19,7 +20,7 @@ export default function Footer() {
           <a className="text-white d-block" style={{textDecoration: 'none'}} href='ContactUs'>Contact Us</a>
         </div>
 
-        <div className="col text-white">
+        <div className="col text-white fw-bold">
           <p className="fw-bold fs-5">Shop</p>
           <a className="text-white d-block" style={{textDecoration: 'none'}} href='/products'>Shop with us</a>
           <a className="text-white d-block" style={{textDecoration: 'none'}} href='/'>Shopping</a>
@@ -27,14 +28,14 @@ export default function Footer() {
           <a className="text-white d-block" style={{textDecoration: 'none'}} href='/terms-of-service'>Terms of Service</a>
         </div>
 
-        <div className="col text-white ">
+        <div className="col text-white fw-bold">
           <p className="fw-bold fs-5">Social</p>
           <a className="text-white d-block" style={{textDecoration: 'none'}} href='https://www.instagram.com/amazonfashionin/'>@instagram </a>
           <a className="text-white d-block" style={{textDecoration: 'none'}} href="https://in.pinterest.com/ideas/women's-clothing/915335281916/">@pinterest</a>
           <a className="text-white d-block" style={{textDecoration: 'none'}} href='https://www.tiktok.com/@amazonfashionin'>@tiktok</a>
         </div>
 
-        <div className="col">
+        <div className="col fw-bold">
             <a href="https://www.instagram.com/amazonfashionin/"><img src={instagramImg} alt="Instagram" className="me-2 m-3 border rounded" style={{width: '27px', height: '27px'}} /></a>
             <a href="https://in.pinterest.com/ideas/women's-clothing/915335281916/"><img src={pinterest} alt="Pinterest" className="me-2 m-3 border rounded" style={{width: '27px', height: '27px'}} /></a>
             <a href=""><img src={tiktokImg} alt="tiktok" className='me-2 m-3 border rounded' style={{width: '27px', height: '27px'}}/></a>
